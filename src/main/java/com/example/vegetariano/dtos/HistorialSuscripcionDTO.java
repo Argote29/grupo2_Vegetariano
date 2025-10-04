354cd0b41762028036b09aa271885dd72cedd7ae
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 13:58:15 -0500

    Update HistorialSuscripcionDTO.java

--- src/main/java/com/example/vegetariano/dtos/HistorialSuscripcionDTO.java
+++ src/main/java/com/example/vegetariano/dtos/HistorialSuscripcionDTO.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 package com.example.vegetariano.dtos;
 
 import java.time.LocalDate;
@@ -44,50 +40,3 @@ public class HistorialSuscripcionDTO {
         this.estado = estado;
     }
 }
-<<<<<<< HEAD
-=======
-=======
-package com.example.vegetariano.dtos;
-
-import java.time.LocalDate;
-
-public class HistorialSuscripcionDTO {
-    private int id_historial;
-    private LocalDate fecha_inico_sub;
-    private LocalDate fecha_final_sub;
-    private String estado;
-
-    public int getId_historial() {
-        return id_historial;
-    }
-
-    public void setId_historial(int id_historial) {
-        this.id_historial = id_historial;
-    }
-
-    public LocalDate getFecha_inico_sub() {
-        return fecha_inico_sub;
-    }
-
-    public void setFecha_inico_sub(LocalDate fecha_inico_sub) {
-        this.fecha_inico_sub = fecha_inico_sub;
-    }
-
-    public LocalDate getFecha_final_sub() {
-        return fecha_final_sub;
-    }
-
-    public void setFecha_final_sub(LocalDate fecha_final_sub) {
-        this.fecha_final_sub = fecha_final_sub;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-}
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
