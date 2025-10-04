a6d4a72a8ec32bed5374433593c083af11b70568
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:08:10 -0500

    Update IPlatoService.java

--- src/main/java/com/example/vegetariano/serviceinterfaces/IPlatoService.java
+++ src/main/java/com/example/vegetariano/serviceinterfaces/IPlatoService.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 package com.example.vegetariano.serviceinterfaces;
 
 import com.example.vegetariano.entities.Plato;
@@ -20,24 +16,4 @@ public interface IPlatoService {
 
 
 }
-<<<<<<< HEAD
-=======
-=======
-package com.example.vegetariano.serviceinterfaces;
 
-import com.example.vegetariano.entities.Plato;
-
-
-import java.util.List;
-
-public interface IPlatoService {
-    public List<Plato> list();
-    public void insert(Plato plato);
-    public void delete(int id);
-    public Plato listId(int id);
-    void update(Plato plato);
-    List<String[]> cantidadIngredientesPorPlato();
-
-}
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
