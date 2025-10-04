519ea1b6520459a7614c19bbed7d407b314e57c2
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:07:48 -0500

    Update IHistorialSuscripcionService.java

--- src/main/java/com/example/vegetariano/serviceinterfaces/IHistorialSuscripcionService.java
+++ src/main/java/com/example/vegetariano/serviceinterfaces/IHistorialSuscripcionService.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 package com.example.vegetariano.serviceinterfaces;
 
 import com.example.vegetariano.entities.HistorialSuscripcion;
@@ -15,21 +11,4 @@ public interface IHistorialSuscripcionService {
     public HistorialSuscripcion listId(int id);
     void update(HistorialSuscripcion historialSuscripcion);
 }
-<<<<<<< HEAD
-=======
-=======
-package com.example.vegetariano.serviceinterfaces;
 
-import com.example.vegetariano.entities.HistorialSuscripcion;
-
-import java.util.List;
-
-public interface IHistorialSuscripcionService {
-    public List<HistorialSuscripcion> list();
-    public void insert(HistorialSuscripcion historialSuscripcion);
-    public void delete(int id);
-    public HistorialSuscripcion listId(int id);
-    void update(HistorialSuscripcion historialSuscripcion);
-}
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
