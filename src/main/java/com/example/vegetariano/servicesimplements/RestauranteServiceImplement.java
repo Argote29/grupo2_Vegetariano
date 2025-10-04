aa1f01b2ca1ca6cda831e42c908ac7ecabe04b52
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:11:49 -0500

    Update RestauranteServiceImplement.java

--- src/main/java/com/example/vegetariano/servicesimplements/RestauranteServiceImplement.java
+++ src/main/java/com/example/vegetariano/servicesimplements/RestauranteServiceImplement.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 package com.example.vegetariano.servicesimplements;
 
 import com.example.vegetariano.entities.Restaurante;
@@ -38,37 +34,3 @@ public class RestauranteServiceImplement implements IRestauranteService {
 
 
 }
-<<<<<<< HEAD
-=======
-=======
-package com.example.vegetariano.servicesimplements;
-
-import com.example.vegetariano.entities.Restaurante;
-import com.example.vegetariano.repositories.IRestauranteRepository;
-import com.example.vegetariano.serviceinterfaces.IRestauranteService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-@Service
-public class RestauranteServiceImplement implements IRestauranteService {
-    @Autowired
-    private IRestauranteRepository rre;
-
-    @Override
-    public List<Restaurante> list() { return rre.findAll(); }
-
-    @Override
-    public void insert(Restaurante restaurante) { rre.save(restaurante); }
-
-    @Override
-    public void delete(int id) { rre.deleteById(id); }
-
-    @Override
-    public Restaurante listId(int id) { return rre.findById(id).orElse(null); }
-
-    @Override
-    public void update(Restaurante restaurante) { rre.save(restaurante); }
-}
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
