6f1105d378bf1d916c75ea2bab0cb67bd2be2c53
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:08:59 -0500

    Update IRolService.java

--- src/main/java/com/example/vegetariano/serviceinterfaces/IRolService.java
+++ src/main/java/com/example/vegetariano/serviceinterfaces/IRolService.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 package com.example.vegetariano.serviceinterfaces;
 
 import com.example.vegetariano.entities.Rol;
@@ -15,21 +11,3 @@ public interface IRolService
     public Rol listId(int id);
     void update(Rol rol);
 }
-<<<<<<< HEAD
-=======
-=======
-package com.example.vegetariano.serviceinterfaces;
-
-import com.example.vegetariano.entities.Rol;
-
-import java.util.List;
-public interface IRolService
-{
-    public List<Rol> list();
-    public void insert(Rol rol);
-    public void delete(int id);
-    public Rol listId(int id);
-    void update(Rol rol);
-}
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
