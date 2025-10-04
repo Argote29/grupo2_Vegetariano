71f7cd2c135d5cac94aca97ba9f0e8ba7a3e62c5
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:07:58 -0500

    Update IIngredientesService.java

--- src/main/java/com/example/vegetariano/serviceinterfaces/IIngredientesService.java
+++ src/main/java/com/example/vegetariano/serviceinterfaces/IIngredientesService.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 package com.example.vegetariano.serviceinterfaces;
 
 import com.example.vegetariano.entities.Ingredientes;
@@ -16,22 +12,3 @@ public interface IIngredientesService {
     public Ingredientes listId(int id);
     void update(Ingredientes ingredientes);
 }
-<<<<<<< HEAD
-=======
-=======
-package com.example.vegetariano.serviceinterfaces;
-
-import com.example.vegetariano.entities.Ingredientes;
-import com.example.vegetariano.entities.Plato;
-
-import java.util.List;
-
-public interface IIngredientesService {
-    public List<Ingredientes> list();
-    public void insert(Ingredientes ingredientes);
-    public void delete(int id);
-    public Ingredientes listId(int id);
-    void update(Ingredientes ingredientes);
-}
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
