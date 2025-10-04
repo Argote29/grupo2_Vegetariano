cebf25f836c112c846af39e2703795be9fb549a6
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:08:48 -0500

    Update IRestauranteService.java

--- src/main/java/com/example/vegetariano/serviceinterfaces/IRestauranteService.java
+++ src/main/java/com/example/vegetariano/serviceinterfaces/IRestauranteService.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 package com.example.vegetariano.serviceinterfaces;
 
 import com.example.vegetariano.entities.Restaurante;
@@ -18,21 +14,4 @@ public interface    IRestauranteService {
     List<String[]> QueryRestaurantePromedioResena();
 
 }
-<<<<<<< HEAD
-=======
-=======
-package com.example.vegetariano.serviceinterfaces;
 
-import com.example.vegetariano.entities.Restaurante;
-
-import java.util.List;
-
-public interface IRestauranteService {
-    public List<Restaurante> list();
-    public void insert(Restaurante restaurante);
-    public void delete(int id);
-    public Restaurante listId(int id);
-    void update(Restaurante restaurante);
-}
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
