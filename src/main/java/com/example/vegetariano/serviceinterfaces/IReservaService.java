b94fb50466a1b786bdbc41733646a6fa61596fba
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:08:39 -0500

    Update IReservaService.java

--- src/main/java/com/example/vegetariano/serviceinterfaces/IReservaService.java
+++ src/main/java/com/example/vegetariano/serviceinterfaces/IReservaService.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 package com.example.vegetariano.serviceinterfaces;
 
 import com.example.vegetariano.entities.Reserva;
@@ -14,20 +10,4 @@ public interface IReservaService {
     public Reserva listId(int id);
     void update(Reserva reserva);
 }
-<<<<<<< HEAD
-=======
-=======
-package com.example.vegetariano.serviceinterfaces;
 
-import com.example.vegetariano.entities.Reserva;
-
-import java.util.List;
-public interface IReservaService {
-    public List<Reserva> list();
-    public void insert(Reserva reserva);
-    public void delete(int id);
-    public Reserva listId(int id);
-    void update(Reserva reserva);
-}
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
