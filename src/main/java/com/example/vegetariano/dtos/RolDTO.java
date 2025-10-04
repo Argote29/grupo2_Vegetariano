3effba74b661c854e5d184f74419ffd8792b37a0
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:02:39 -0500

    Update RolDTO.java

--- src/main/java/com/example/vegetariano/dtos/RolDTO.java
+++ src/main/java/com/example/vegetariano/dtos/RolDTO.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 package com.example.vegetariano.dtos;
 
 import jakarta.persistence.Column;
@@ -36,42 +32,4 @@ public class    RolDTO {
         this.descripcion = descripcion;
     }
 }
-<<<<<<< HEAD
-=======
-=======
-package com.example.vegetariano.dtos;
 
-import jakarta.persistence.Column;
-
-public class    RolDTO {
-
-    private int id_rol;
-    private String nombre;
-    private String descripcion;
-
-    public int getId_rol() {
-        return id_rol;
-    }
-
-    public void setId_rol(int id_rol) {
-        this.id_rol = id_rol;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-}
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
