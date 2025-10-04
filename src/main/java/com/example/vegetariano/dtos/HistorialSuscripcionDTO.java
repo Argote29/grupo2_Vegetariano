3a5daabb9b97ceca4f63a5f05165dc8363e41feb
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:53:47 -0500

    Update HistorialSuscripcionDTO.java

--- src/main/java/com/example/vegetariano/dtos/HistorialSuscripcionDTO.java
+++ src/main/java/com/example/vegetariano/dtos/HistorialSuscripcionDTO.java
@@ -7,36 +7,21 @@ public class HistorialSuscripcionDTO {
     private LocalDate fecha_inico_sub;
     private LocalDate fecha_final_sub;
     private String estado;
+    private Integer id_usuario;
 
-    public int getId_historial() {
-        return id_historial;
-    }
+    public int getId_historial() { return id_historial; }
+    public void setId_historial(int id_historial) { this.id_historial = id_historial; }
 
-    public void setId_historial(int id_historial) {
-        this.id_historial = id_historial;
-    }
+    public LocalDate getFecha_inico_sub() { return fecha_inico_sub; }
+    public void setFecha_inico_sub(LocalDate fecha_inicio_sub) { this.fecha_inico_sub = fecha_inicio_sub; }
 
-    public LocalDate getFecha_inico_sub() {
-        return fecha_inico_sub;
-    }
+    public LocalDate getFecha_final_sub() { return fecha_final_sub; }
+    public void setFecha_final_sub(LocalDate fecha_final_sub) { this.fecha_final_sub = fecha_final_sub; }
 
-    public void setFecha_inico_sub(LocalDate fecha_inico_sub) {
-        this.fecha_inico_sub = fecha_inico_sub;
-    }
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
 
-    public LocalDate getFecha_final_sub() {
-        return fecha_final_sub;
-    }
-
-    public void setFecha_final_sub(LocalDate fecha_final_sub) {
-        this.fecha_final_sub = fecha_final_sub;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
+    public Integer getId_usuario() { return id_usuario; }
+    public void setId_usuario(Integer id_usuario) { this.id_usuario = id_usuario; }
 }
+
