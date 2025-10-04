8950631b9d3022b4205faf49fd2ad4828db217cd
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:05:52 -0500

    Update IResenaRepository.java

--- src/main/java/com/example/vegetariano/repositories/IResenaRepository.java
+++ src/main/java/com/example/vegetariano/repositories/IResenaRepository.java
@@ -1,26 +1,10 @@
 package com.example.vegetariano.repositories;
 
 
-<<<<<<< HEAD
 import com.example.vegetariano.entities.Resena;
-=======
-<<<<<<< HEAD
-import com.example.vegetariano.entities.Resena;
-=======
-import com.example.vegetariano.entities.Reseña;
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-<<<<<<< HEAD
 public interface IResenaRepository extends JpaRepository<Resena,Integer> {
-=======
-<<<<<<< HEAD
-public interface IResenaRepository extends JpaRepository<Resena,Integer> {
-=======
-public interface IResenaRepository extends JpaRepository<Reseña,Integer> {
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 }
