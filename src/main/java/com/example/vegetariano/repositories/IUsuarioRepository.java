baf52f3cf2d0ae786caf28e3427f670d569e5116
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:06:35 -0500

    Update IUsuarioRepository.java

--- src/main/java/com/example/vegetariano/repositories/IUsuarioRepository.java
+++ src/main/java/com/example/vegetariano/repositories/IUsuarioRepository.java
@@ -2,10 +2,6 @@ package com.example.vegetariano.repositories;
 
 import com.example.vegetariano.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -22,14 +18,5 @@ public interface IUsuarioRepository extends JpaRepository<Usuario,Integer>
     List<String[]> QueryReservaUsuario();
     Optional<Usuario> findByCorreo(String correo);
 
-<<<<<<< HEAD
-=======
-=======
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IUsuarioRepository extends JpaRepository<Usuario,Integer>
-{
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 }
+
