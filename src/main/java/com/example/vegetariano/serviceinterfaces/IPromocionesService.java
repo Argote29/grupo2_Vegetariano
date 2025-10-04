c4f4fcb0ec63bda1a2c6482f623fb8de447fa91a
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:08:20 -0500

    Update IPromocionesService.java

--- src/main/java/com/example/vegetariano/serviceinterfaces/IPromocionesService.java
+++ src/main/java/com/example/vegetariano/serviceinterfaces/IPromocionesService.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 package com.example.vegetariano.serviceinterfaces;
 
 import com.example.vegetariano.entities.Promociones;
@@ -15,21 +11,3 @@ public interface IPromocionesService {
     public Promociones listId(int id);
     void update(Promociones promociones);
 }
-<<<<<<< HEAD
-=======
-=======
-package com.example.vegetariano.serviceinterfaces;
-
-import com.example.vegetariano.entities.Promociones;
-
-import java.util.List;
-
-public interface IPromocionesService {
-    public List<Promociones> list();
-    public void insert(Promociones promociones);
-    public void delete(int id);
-    public Promociones listId(int id);
-    void update(Promociones promociones);
-}
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
