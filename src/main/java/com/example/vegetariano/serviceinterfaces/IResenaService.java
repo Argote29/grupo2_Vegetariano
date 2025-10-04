17f87729133143a5a52d4121696f54e0d0cd90e1
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:08:30 -0500

    Update IResenaService.java

--- src/main/java/com/example/vegetariano/serviceinterfaces/IResenaService.java
+++ src/main/java/com/example/vegetariano/serviceinterfaces/IResenaService.java
@@ -1,36 +1,15 @@
 package com.example.vegetariano.serviceinterfaces;
 
-<<<<<<< HEAD
 import com.example.vegetariano.entities.Resena;
-=======
-<<<<<<< HEAD
-import com.example.vegetariano.entities.Resena;
-=======
-import com.example.vegetariano.entities.Reseña;
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 
 import java.util.List;
 
 public interface IResenaService {
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
     public List<Resena> list();
     public void insert(Resena resena);
     public void delete(int id);
     public Resena listId(int id);
     void update(Resena resena);
-<<<<<<< HEAD
-=======
-=======
-    public List<Reseña> list();
-    public void insert(Reseña reseña);
-    public void delete(int id);
-    public Reseña listId(int id);
-    void update(Reseña reseña);
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 }
+
