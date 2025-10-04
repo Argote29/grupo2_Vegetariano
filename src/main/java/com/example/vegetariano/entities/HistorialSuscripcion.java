b11be60be9ba21e7bff3736bd26bcd9cc7f9e03e
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:03:14 -0500

    Update HistorialSuscripcion.java

--- src/main/java/com/example/vegetariano/entities/HistorialSuscripcion.java
+++ src/main/java/com/example/vegetariano/entities/HistorialSuscripcion.java
@@ -19,15 +19,7 @@ public class HistorialSuscripcion {
     private LocalDate fecha_final_sub;
 
     @Column(name = "estado", nullable = false, length = 70)
-<<<<<<< HEAD
     private boolean estado;
-=======
-<<<<<<< HEAD
-    private boolean estado;
-=======
-    private String estado;
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 
     @ManyToOne
     @JoinColumn(name = "id_usuario", nullable = false)
@@ -35,15 +27,7 @@ public class HistorialSuscripcion {
 
     public HistorialSuscripcion () {}
 
-<<<<<<< HEAD
     public HistorialSuscripcion(int id_historial, LocalDate fecha_inico_sub, boolean estado, LocalDate fecha_final_sub, Usuario usuario) {
-=======
-<<<<<<< HEAD
-    public HistorialSuscripcion(int id_historial, LocalDate fecha_inico_sub, boolean estado, LocalDate fecha_final_sub, Usuario usuario) {
-=======
-    public HistorialSuscripcion(int id_historial, LocalDate fecha_inico_sub, String estado, LocalDate fecha_final_sub, Usuario usuario) {
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
         this.id_historial = id_historial;
         this.fecha_inico_sub = fecha_inico_sub;
         this.estado = estado;
@@ -75,25 +59,11 @@ public class HistorialSuscripcion {
         this.fecha_final_sub = fecha_final_sub;
     }
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
     public boolean getEstado() {
         return estado;
     }
 
     public void setEstado(boolean estado) {
-<<<<<<< HEAD
-=======
-=======
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
         this.estado = estado;
     }
 
