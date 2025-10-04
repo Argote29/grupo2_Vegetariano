a4969fa8b2d36b69a91b559db5b6e0d73f150bd1
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-10-04 14:04:59 -0500

    Update IHistorialSuscripcionRepository.java

--- src/main/java/com/example/vegetariano/repositories/IHistorialSuscripcionRepository.java
+++ src/main/java/com/example/vegetariano/repositories/IHistorialSuscripcionRepository.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
 package com.example.vegetariano.repositories;
 
 import com.example.vegetariano.entities.HistorialSuscripcion;
@@ -11,17 +7,3 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IHistorialSuscripcionRepository extends JpaRepository<HistorialSuscripcion,Integer> {
 }
-<<<<<<< HEAD
-=======
-=======
-package com.example.vegetariano.repositories;
-
-import com.example.vegetariano.entities.HistorialSuscripcion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IHistorialSuscripcionRepository extends JpaRepository<HistorialSuscripcion,Integer> {
-}
->>>>>>> e1645271619c55ea2f508aa7ec1c23a1f4672a86
->>>>>>> f44f6e44411c9355e2be20fb1804fc3c674c6497
