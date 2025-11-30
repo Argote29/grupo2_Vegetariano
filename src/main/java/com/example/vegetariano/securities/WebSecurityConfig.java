6db5f722dbbcd2c168dd3cb70b17536266a6bb47
Alessandro Del Piero Fabian Argote Bendezu <136865815+Argote29@users.noreply.github.com>
2025-11-30 15:03:58 -0500

    Update WebSecurityConfig.java

--- src/main/java/com/example/vegetariano/securities/WebSecurityConfig.java
+++ src/main/java/com/example/vegetariano/securities/WebSecurityConfig.java
@@ -71,7 +71,7 @@ public class WebSecurityConfig {
                                 "/webjars/**",
                                 "/roles/**",
                                 "/agruparReservaUsuario",
-                                "/usuarios"
+                                "/usuarios/**"
 
                         ).permitAll()
 
